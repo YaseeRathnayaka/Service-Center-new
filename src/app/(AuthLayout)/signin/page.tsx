68b29c4c592e0eff8f66c518cbe7864b1783664b
Cvan <infocvanguard@gmail.com>
2025-07-10 00:13:00 +0530

    Update page.tsx

--- src/app/(AuthLayout)/signin/page.tsx
+++ src/app/(AuthLayout)/signin/page.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import AuthLayout from "../AuthLayout";
 import AuthForm from "../../../components/auth/AuthForm";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { firebaseApp } from "../../../firebaseConfig";
 
 export default function SignInPage() {
+  const router = useRouter();
+  useEffect(() => {
+    const auth = getAuth(firebaseApp);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        router.replace("/dashboard");
+      }
+    });
+    return () => unsubscribe();
+  }, [router]);
+
   return (
     <AuthLayout>
       <AuthForm mode="login" />
